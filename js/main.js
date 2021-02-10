99f927138a35d5377458c3644d2280871b2a2e7f
Tanja <tanja120a@gmail.com>
2021-02-10 08:01:18 +0100

    UI var

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
+//List of tables -8
 let tables = [];
+//list of active orders
 let orders = [];
-const createOrderBtn = document.querySelector('.create-order-btn');
+//number of last created order
+let currentNumber = 0;
+//name of current item
+let currentName = "";
 const tableList = document.querySelector(".table-list");
 const ordersList = document.getElementById("orders-list");
-
+const nameInput = document.getElementById("name-input");
+//const createMealBtn = document.querySelector(".create-meal-btn");
+//const createBeverageBtn = document.querySelector(".create-beverage-btn");
 class Order{
-    constructor(tableNumber){
+    constructor(tableNumber, numberOfItems){
         this.date = this.getDate();
-        //this.numberOfOrder = Order.temporaryNumber(number);
-        this.tableNumber = tableNumber
+        this.orderNumber = Order.getNumber();
+        this.tableNumber = tableNumber;
     }
     getDate(){
         let day = new Date().getDate();
@@ -20,37 +27,59 @@ class Order{
         let t = time + ":" + minutes;
         return [d, t];
     }
-    
+    static getNumber(){
+        currentNumber = ++currentNumber;
+        return currentNumber;
+    }
+    static getInput(){
+       currentName =  nameInput.value;
+    }
+    static clearInput(){
+        currentName = "";
+        nameInput.value = "";
+    }
 }
 class Table{
     constructor(table){
         this.tableName = table;
     }
-    static createOrder(tableNumber){
-        const order = new Order(tableNumber);
-        orders.push(order);      
-        Table.displayOrders(orders);  
-    }
-
-    static displayOrders(){
-        let div = document.createElement("div");
-        div.setAttribute("class", "order");
-        orders.map((order) => {
-            
-            
-            div.innerHTML = `
-            <div class="new-order">    
-                <p>Table number: ${order.tableNumber}</p>
-                <p>Date: ${order.date}</p>
-            </div>
-            `;
-            
-        })
-        ordersList.appendChild(div);
-    }
-   
 }
-class Tables{
+class Beverage{
+    constructor(type, name, volume){
+        this.type = type;
+        this.name = name;
+        this.price = this.getPrice();
+        this.volume = volume;
+    }
+    getPrice(){
+        const price =Math.floor(Math.random()*350)+150;
+        return price;
+    }
+}
+class Pizza{
+    constructor(name){
+        this.type = "Pizza";
+        this.name = name;
+        this.price = this.getPrice();
+    }
+     getPrice(){
+        const price = Math.floor(Math.random()*301)+300;
+        return price
+    }
+}
+class Pasta{
+    constructor(name){
+        this.type = "Pasta";
+        this.name = name;
+        this.price = this.getPrice();
+    }
+    getPrice(){
+        const price = Math.floor(Math.random()*301) + 300;
+        return price;
+    }
+}
+class Restaurant{
+    //create Array of tables
     static getTables(numb){
         for(let i = 1; i <= numb; i++){
         tables.push(new Table(i));
@@ -60,58 +89,37 @@ class Tables{
             div.setAttribute("class", "table");
             div.setAttribute("id", table.tableName);
             div.innerHTML = "Table No" + table.tableName;
-            div.addEventListener("click",(e) => Table.createOrder(e.target.id));
+            div.addEventListener("click",(e) => Restaurant.createOrder(e.target.id));
             tableList.appendChild(div);
         })
-        console.log(tables)
+    }
+    static createOrder(tableNumber){
+        const order = new Order(tableNumber);
+        orders.push(order);      
+        UI.displayOrders(orders);  
+    }   
+
+}
+class UI{
+    static displayOrders(){
+        let div = document.createElement("div");
+        div.setAttribute("class", "order");
+        orders.map((order) => {            
+            div.innerHTML = `
+            <div class="new-order">    
+                <h3>Order number: ${order.orderNumber}</h3>
+                <p>Table number: ${order.tableNumber}</p>
+                <p>Date: ${order.date}</p>
+                <input type="text" placeholder="name of meal"/>
+            </div>
+            `;            
+        })
+        ordersList.appendChild(div);
     }
 }
+document.addEventListener('DOMContentLoaded', Restaurant.getTables(8));
+//console.log(new Pizza("Capriciosa"))
 
-document.addEventListener('DOMContentLoaded', Tables.getTables(8));
-/*class Order{
-    constructor(){
-        this.date = this.getDate();
-        this.numberOfOrder = Order.temporaryNumber(number);
-    }    
-    getDate(){
-        let day = new Date().getDate();
-        let month = new Date().getMonth() + 1;
-        let year = new Date().getFullYear();
-        let time = new Date().getHours();
-        let minutes = new Date().getMinutes();
-        let d = day + "/" + month + "/" + year;
-        let t = time + ":" + minutes;
-        return [d, t];
-    }
-    get numbOfOrder() {
-        return this.number;
-    }
-    set numbOfOrder(number) {
-        this.number = ++number;
-    }
-    static temporaryNumber(number = 0){   
-        number = ++number;
-        return number;
-    }
-
-    static createOrder(x){  
-
-        document.getElementById("order").innerHTML = `
-        <div class="new-order">
-            <h3>Order number: ${x.number}</h3>
-            <p>Date: ${x.date}</p>
-        </div>
-        `;
-    }
-    
-};*/
-/*createOrderBtn.addEventListener('click',() => {
-    let order = new Order();
-    order.getDate();
-    order.numbOfOrder = order.number;
-    console.log(order)
-    return order
-})*/
 /*class Billing extends Order{
     constructor(cached){
         super(number)
